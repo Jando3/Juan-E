8e6d1cd05fe2e48f6612728ae8f3222ae153182d
Jando3 <JSand03330@gmail.com>
2023-02-27 22:03:15 -0500

    fixed link to server

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,7 +21,7 @@ const CreatePost = () => {
         if (form.prompt) {
             try {
                 setGeneratingImg(true);
-                const response = await fetch('https://juan-e.onrender.com', {
+                const response = await fetch('https://juan-e.onrender.com/api/v1/dalle', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
@@ -52,7 +52,7 @@ const CreatePost = () => {
         if (form.prompt && form.photo) {
             setLoading(true);
             try {
-                const response = await fetch('https://juan-e.onrender.com',
+                const response = await fetch('https://juan-e.onrender.com/api/v1/post',
                     {
                         method: 'POST',
                         headers: {
